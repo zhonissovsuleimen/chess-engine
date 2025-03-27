e1a0879fb9786d2485fb4f7b377555177d9c0cc2
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-03-27 07:44:22 +0500

    fen pieces parsing

--- src/board.rs
+++ src/board.rs
@@ -1,6 +1,64 @@
 use crate::pieces::Pieces;
 
-struct Board {
+pub struct Board {
   white: Pieces,
   black: Pieces,
+}
+
+impl Board {
+  pub fn from_fen(fen_string: &str) -> Board {
+    let mut board = Board {
+      white: Pieces::new(),
+      black: Pieces::new(),
+    };
+
+    let slices: Vec<&str> = fen_string.split_whitespace().collect();
+    assert_eq!(slices.len(), 6);
+
+    let pieces = slices[0];
+
+    let mut rank = 7;
+    let mut file = 0;
+    for c in pieces.chars() {
+      let mut increment_file = true;
+
+      let bit_shift = (7 - rank) * 8 + file;
+      match c {
+        'P' => board.white.pawns += 1 << bit_shift,
+        'N' => board.white.knights += 1 << bit_shift,
+        'B' => board.white.bishops += 1 << bit_shift,
+        'R' => board.white.rooks += 1 << bit_shift,
+        'Q' => board.white.queens += 1 << bit_shift,
+        'K' => board.white.king = 1 << bit_shift,
+        'p' => board.black.pawns += 1 << bit_shift,
+        'n' => board.black.knights += 1 << bit_shift,
+        'b' => board.black.bishops += 1 << bit_shift,
+        'r' => board.black.rooks += 1 << bit_shift,
+        'q' => board.black.queens += 1 << bit_shift,
+        'k' => board.black.king = 1 << bit_shift,
+        digit if c.is_ascii_digit() => {
+          assert_ne!(digit, '0');
+          assert_ne!(digit, '9');
+
+          file += digit.to_digit(10).unwrap();
+          increment_file = false;
+        }
+        '/' => {
+          rank -= 1;
+          file = 0;
+
+          increment_file = false;
+        }
+        _ => {
+          increment_file = false;
+        }
+      }
+
+      if increment_file {
+        file += 1;
+      }
+    }
+
+    board
+  }
 }
