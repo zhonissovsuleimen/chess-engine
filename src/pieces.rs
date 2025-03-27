5a0723bc2ca8202687ae8f857c9b0530048b9a3b
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-03-27 07:53:02 +0500

    default black and white pieces

--- src/pieces.rs
+++ src/pieces.rs
@@ -12,4 +12,12 @@ impl Pieces {
   pub fn empty() -> Pieces {
     Pieces { pawns: 0, knights: 0, bishops: 0, rooks: 0, queens: 0, king: 0 }
   }
+
+  pub fn white() -> Pieces {
+    Pieces { pawns: 71776119061217280, knights: 4755801206503243776, bishops: 2594073385365405696, rooks: 9295429630892703744, queens: 576460752303423488, king: 1152921504606846976 }
+  }
+
+  pub fn black() -> Pieces {
+    Pieces { pawns: 65280, knights: 66, bishops: 36, rooks: 129, queens: 8, king: 16 }
+  }
 }
