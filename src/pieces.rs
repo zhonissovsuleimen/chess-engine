43343076ddeae372a08fd55c22da5a5ca4753531
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-03-27 07:45:57 +0500

    pieces new fn

--- src/pieces.rs
+++ src/pieces.rs
@@ -1,8 +1,15 @@
+// rank 8 file a is bit 0, rank 1 file h is bit 63
 pub struct Pieces {
-  pawns: u64,
-  knights: u64,
-  bishops: u64,
-  rooks: u64,
-  queens: u64,
-  king: u64
+  pub pawns: u64,
+  pub knights: u64,
+  pub bishops: u64,
+  pub rooks: u64,
+  pub queens: u64,
+  pub king: u64
+}
+
+impl Pieces {
+  pub fn new() -> Pieces {
+    Pieces { pawns: 0, knights: 0, bishops: 0, rooks: 0, queens: 0, king: 0 }
+  }
 }
