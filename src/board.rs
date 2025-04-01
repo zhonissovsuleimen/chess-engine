45dc7ac7cf67b64cc9de15df8d96eb798bc92a42
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-04-01 09:18:09 +0500

    splitting stuff

--- src/board.rs
+++ src/board.rs
@@ -1,3 +1,5 @@
+//TODO: castiling, pin, checks, checkmates?
+
 use crate::pieces::Pieces;
 use bevy::ecs::system::Resource;
 
@@ -14,6 +16,7 @@ pub struct Board {
   en_pessant_mask: u64,
 }
 
+//constructor
 impl Board {
   pub fn empty() -> Board {
     let mut board = Board {
@@ -49,6 +52,69 @@ impl Board {
     board
   }
 
+  pub fn from_fen(fen_string: &str) -> Board {
+    let mut board = Board::empty();
+
+    let slices: Vec<&str> = fen_string.split_whitespace().collect();
+    assert_eq!(slices.len(), 6);
+
+    let pieces = slices[0];
+
+    let mut rank = 7;
+    let mut file = 0;
+    for c in pieces.chars() {
+      let mut increment_file = true;
+
+      let bit_shift = (7 - rank) * 8 + file;
+      match c {
+        'P' => board.white.pawns += 1 << bit_shift,
+        'N' => board.white.knights += 1 << bit_shift,
+        'B' => board.white.bishops += 1 << bit_shift,
+        'R' => board.white.rooks += 1 << bit_shift,
+        'Q' => board.white.queens += 1 << bit_shift,
+        'K' => board.white.king = 1 << bit_shift,
+        'p' => board.black.pawns += 1 << bit_shift,
+        'n' => board.black.knights += 1 << bit_shift,
+        'b' => board.black.bishops += 1 << bit_shift,
+        'r' => board.black.rooks += 1 << bit_shift,
+        'q' => board.black.queens += 1 << bit_shift,
+        'k' => board.black.king = 1 << bit_shift,
+        digit if c.is_ascii_digit() => {
+          assert_ne!(digit, '0');
+          assert_ne!(digit, '9');
+
+          file += digit.to_digit(10).unwrap();
+          increment_file = false;
+        }
+        '/' => {
+          rank -= 1;
+          file = 0;
+
+          increment_file = false;
+        }
+        wrong_char => {
+          panic!("Unexpected character ({wrong_char}) in piece placement data");
+        }
+      }
+
+      if increment_file {
+        file += 1;
+      }
+    }
+
+    match slices[1] {
+      "w" => board.white_to_move = true,
+      "b" => board.white_to_move = false,
+      wrong_char => panic!("Unexpected character ({wrong_char}) in active color data"),
+    }
+
+    board.update_masks();
+    board
+  }
+}
+
+//moving/updating
+impl Board {
   pub fn move_piece(&mut self, from_id: usize, to_id: usize) -> bool {
     assert!(from_id < 64);
     assert!(to_id < 64);
@@ -126,31 +192,10 @@ impl Board {
       | !self.white_turn_mask & self.white.pieces_concat();
     self.advance_mask = self.white.pawns_advance | self.black.pawns_advance;
   }
+}
 
-  fn is_pawn(&self, at_mask: u64) -> bool {
-    return self.white.is_pawn(at_mask) || self.black.is_pawn(at_mask);
-  }
-
-  fn is_knight(&self, at_mask: u64) -> bool {
-    return self.white.is_knight(at_mask) || self.black.is_knight(at_mask);
-  }
-
-  fn is_bishop(&self, at_mask: u64) -> bool {
-    return self.white.is_bishop(at_mask) || self.black.is_bishop(at_mask);
-  }
-
-  fn is_rook(&self, at_mask: u64) -> bool {
-    return self.white.is_rook(at_mask) || self.black.is_rook(at_mask);
-  }
-
-  fn is_queen(&self, at_mask: u64) -> bool {
-    return self.white.is_queen(at_mask) || self.black.is_queen(at_mask);
-  }
-
-  fn is_king(&self, at_mask: u64) -> bool {
-    return self.white.is_king(at_mask) || self.black.is_king(at_mask);
-  }
-
+//move generations
+impl Board {
   fn gen_pawn_moves(&self, at_mask: u64) -> u64 {
     let mut pawn_move;
 
@@ -331,68 +376,35 @@ impl Board {
       current = (current.0 + dx, current.1 + dy);
     }
   }
+}
+
+//state
+impl Board {
+  fn is_pawn(&self, at_mask: u64) -> bool {
+    return self.white.is_pawn(at_mask) || self.black.is_pawn(at_mask);
+  }
+
+  fn is_knight(&self, at_mask: u64) -> bool {
+    return self.white.is_knight(at_mask) || self.black.is_knight(at_mask);
+  }
+
+  fn is_bishop(&self, at_mask: u64) -> bool {
+    return self.white.is_bishop(at_mask) || self.black.is_bishop(at_mask);
+  }
+
+  fn is_rook(&self, at_mask: u64) -> bool {
+    return self.white.is_rook(at_mask) || self.black.is_rook(at_mask);
+  }
+
+  fn is_queen(&self, at_mask: u64) -> bool {
+    return self.white.is_queen(at_mask) || self.black.is_queen(at_mask);
+  }
+
+  fn is_king(&self, at_mask: u64) -> bool {
+    return self.white.is_king(at_mask) || self.black.is_king(at_mask);
+  }
 
   pub fn get_piece_delta(&self) -> i32 {
     self.white.get_value() - self.black.get_value()
-  }
-
-  pub fn from_fen(fen_string: &str) -> Board {
-    let mut board = Board::empty();
-
-    let slices: Vec<&str> = fen_string.split_whitespace().collect();
-    assert_eq!(slices.len(), 6);
-
-    let pieces = slices[0];
-
-    let mut rank = 7;
-    let mut file = 0;
-    for c in pieces.chars() {
-      let mut increment_file = true;
-
-      let bit_shift = (7 - rank) * 8 + file;
-      match c {
-        'P' => board.white.pawns += 1 << bit_shift,
-        'N' => board.white.knights += 1 << bit_shift,
-        'B' => board.white.bishops += 1 << bit_shift,
-        'R' => board.white.rooks += 1 << bit_shift,
-        'Q' => board.white.queens += 1 << bit_shift,
-        'K' => board.white.king = 1 << bit_shift,
-        'p' => board.black.pawns += 1 << bit_shift,
-        'n' => board.black.knights += 1 << bit_shift,
-        'b' => board.black.bishops += 1 << bit_shift,
-        'r' => board.black.rooks += 1 << bit_shift,
-        'q' => board.black.queens += 1 << bit_shift,
-        'k' => board.black.king = 1 << bit_shift,
-        digit if c.is_ascii_digit() => {
-          assert_ne!(digit, '0');
-          assert_ne!(digit, '9');
-
-          file += digit.to_digit(10).unwrap();
-          increment_file = false;
-        }
-        '/' => {
-          rank -= 1;
-          file = 0;
-
-          increment_file = false;
-        }
-        wrong_char => {
-          panic!("Unexpected character ({wrong_char}) in piece placement data");
-        }
-      }
-
-      if increment_file {
-        file += 1;
-      }
-    }
-
-    match slices[1] {
-      "w" => board.white_to_move = true,
-      "b" => board.white_to_move = false,
-      wrong_char => panic!("Unexpected character ({wrong_char}) in active color data"),
-    }
-
-    board.update_masks();
-    board
   }
 }
