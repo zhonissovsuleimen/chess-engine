5dc93675c3a2da7d065b9f21eb67987d7d3996cf
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-04-13 13:54:30 +0500

    from_fen & todo update

--- src/board/board.rs
+++ src/board/board.rs
@@ -1,4 +1,4 @@
-//TODO: changing game status, from_fen update
+//TODO: changing game status, full/half time clocks, fifty-move rule
 use super::{
   board_movement_trait::BoardMovement, move_generation::Modifier, pieces::Pieces,
   util_fns::*,
@@ -75,6 +75,7 @@ impl Board {
     let slices: Vec<&str> = fen_string.split_whitespace().collect();
     assert_eq!(slices.len(), 6);
 
+    //pieces
     let pieces = slices[0];
 
     let mut rank = 7;
@@ -83,19 +84,20 @@ impl Board {
       let mut increment_file = true;
 
       let bit_shift = (7 - rank) * 8 + file;
+      let pos = 1 << bit_shift;
       match c {
-        'P' => board.white.pawns += 1 << bit_shift,
-        'N' => board.white.knights += 1 << bit_shift,
-        'B' => board.white.bishops += 1 << bit_shift,
-        'R' => board.white.rooks += 1 << bit_shift,
-        'Q' => board.white.queens += 1 << bit_shift,
-        'K' => board.white.king = 1 << bit_shift,
-        'p' => board.black.pawns += 1 << bit_shift,
-        'n' => board.black.knights += 1 << bit_shift,
-        'b' => board.black.bishops += 1 << bit_shift,
-        'r' => board.black.rooks += 1 << bit_shift,
-        'q' => board.black.queens += 1 << bit_shift,
-        'k' => board.black.king = 1 << bit_shift,
+        'P' => board.white.pawns |= pos,
+        'N' => board.white.knights |= pos,
+        'B' => board.white.bishops |= pos,
+        'R' => board.white.rooks |= pos,
+        'Q' => board.white.queens |= pos,
+        'K' => board.white.king = pos,
+        'p' => board.black.pawns |= pos,
+        'n' => board.black.knights |= pos,
+        'b' => board.black.bishops |= pos,
+        'r' => board.black.rooks |= pos,
+        'q' => board.black.queens |= pos,
+        'k' => board.black.king = pos,
         digit if c.is_ascii_digit() => {
           assert_ne!(digit, '0');
           assert_ne!(digit, '9');
@@ -119,6 +121,11 @@ impl Board {
       }
     }
 
+    //advance mask
+    board.advance_mask = board.white.pawns & 0x00_FF_00_00_00_00_00_00;
+    board.advance_mask |= board.black.pawns & 0x00_00_00_00_00_00_FF_00;
+
+    //turn
     match slices[1] {
       "w" => board.white_turn = true,
       "b" => board.white_turn = false,
@@ -127,6 +134,22 @@ impl Board {
       }
     }
 
+    //castling
+    if slices[2] != "-" {
+      let chars = slices[2].chars().collect::<Vec<char>>();
+
+      for c in chars {
+        match c {
+          'K' => board.castling_mask |= 0x90_00_00_00_00_00_00_00,
+          'Q' => board.castling_mask |= 0x11_00_00_00_00_00_00_00,
+          'k' => board.castling_mask |= 0x00_00_00_00_00_00_00_90,
+          'q' => board.castling_mask |= 0x00_00_00_00_00_00_00_11,
+          wrong_char => panic!("Unexpected character ({wrong_char}) in castling rights data")
+        }
+      }
+    }
+
+    //en passant
     if slices[3] != "-" {
       let enp_chars = slices[3].chars().collect::<Vec<char>>();
       assert_eq!(enp_chars.len(), 2);
