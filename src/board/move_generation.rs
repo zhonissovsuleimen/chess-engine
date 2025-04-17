9651bc81ee94389e004065550ca30d320df05430
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-04-18 00:15:04 +0500

    new modifier

--- src/board/move_generation.rs
+++ src/board/move_generation.rs
@@ -1,7 +1,7 @@
 use super::{
   Board,
   board_movement_trait::BoardMovement,
-  move_generation_modifiers::{ALLY_IS_ENEMY, NONE, ally_is_enemy, empty_is_enemy},
+  move_generation_modifiers::{ALLY_IS_ENEMY, NONE, ally_is_enemy, empty_is_enemy, king_is_empty},
   util_fns::*,
 };
 
@@ -40,9 +40,13 @@ impl Board {
   }
 
   pub(super) fn gen_pawn_capturing_moves(&self, at_mask: u64, modifier: u64) -> u64 {
-    let mut enemy_mask = self.enemy;
-    enemy_mask |= mask_from_bool(empty_is_enemy(modifier)) & self.empty;
-    enemy_mask |= mask_from_bool(ally_is_enemy(modifier)) & self.ally;
+    let mut empty = self.empty;
+    empty |= mask_from_bool(king_is_empty(modifier)) & self.ally_king;
+
+    let mut enemy = self.enemy;
+    enemy |= mask_from_bool(empty_is_enemy(modifier)) & self.empty;
+    enemy |= mask_from_bool(ally_is_enemy(modifier)) & self.ally;
+    enemy &= !(mask_from_bool(king_is_empty(modifier)) & self.ally_king);
 
     let one_move_up = at_mask.move_up_mask(1);
     let one_move_down = at_mask.move_down_mask(1);
@@ -58,7 +62,7 @@ impl Board {
       one_move_down.move_right_mask(1),
     );
 
-    (enemy_to_left | enemy_to_right) & (enemy_mask | self.en_passant_mask)
+    (enemy_to_left | enemy_to_right) & (enemy | self.en_passant_mask)
   }
 
   pub(super) fn gen_knight_moves(&self, at_mask: u64, modifier: u64) -> u64 {
@@ -127,8 +131,13 @@ impl Board {
     let long_empty = at_mask
       & self.empty.move_right_mask(1)
       & self.empty.move_right_mask(2)
-      & self.empty.move_right_mask(3) > 0;
-    let long_safe = at_mask & !self.under_attack & !self.under_attack.move_right_mask(2) > 0;
+      & self.empty.move_right_mask(3)
+      > 0;
+    let long_safe = at_mask
+      & !self.king_danger
+      & !self.king_danger.move_right_mask(1)
+      & !self.king_danger.move_right_mask(2)
+      > 0;
 
     let new_pos = at_mask.move_left_mask(2);
     new_pos & mask_from_bool(long_rights && long_empty && long_safe)
@@ -139,10 +148,12 @@ impl Board {
     let short_rook = rooks & at_mask.move_right_mask(3);
 
     let short_rights = self.castling_mask & short_rook > 0 && self.castling_mask & at_mask > 0;
-    let short_empty = at_mask 
-      & self.empty.move_left_mask(1) 
-      & self.empty.move_left_mask(2) > 0;
-    let short_safe = at_mask & !self.under_attack & !self.under_attack.move_left_mask(2) > 0;
+    let short_empty = at_mask & self.empty.move_left_mask(1) & self.empty.move_left_mask(2) > 0;
+    let short_safe = at_mask
+      & !self.king_danger
+      & self.king_danger.move_left_mask(1)
+      & !self.king_danger.move_left_mask(2)
+      > 0;
 
     let new_pos = at_mask.move_right_mask(2);
     new_pos & mask_from_bool(short_rights && short_empty && short_safe)
@@ -154,9 +165,13 @@ impl Board {
     offsets: Vec<(i32, i32)>,
     modifier: u64,
   ) -> u64 {
-    let mut enemy_mask = self.enemy;
-    enemy_mask |= mask_from_bool(empty_is_enemy(modifier)) & self.empty;
-    enemy_mask |= mask_from_bool(ally_is_enemy(modifier)) & self.ally;
+    let mut empty = self.empty;
+    empty |= mask_from_bool(king_is_empty(modifier)) & self.ally_king;
+
+    let mut enemy = self.enemy;
+    enemy |= mask_from_bool(empty_is_enemy(modifier)) & self.empty;
+    enemy |= mask_from_bool(ally_is_enemy(modifier)) & self.ally;
+    enemy &= !(mask_from_bool(king_is_empty(modifier)) & self.ally_king);
 
     let mut moves = 0;
 
@@ -173,16 +188,20 @@ impl Board {
         new_move.move_down_mask(dy.unsigned_abs()),
       );
 
-      moves |= new_move & (self.empty | enemy_mask);
+      moves |= new_move & (empty | enemy);
     }
 
     moves
   }
 
   fn gen_iterative_moves(&self, at_mask: u64, dx: i32, dy: i32, modifier: u64) -> u64 {
-    let mut enemy_mask = self.enemy;
-    enemy_mask |= mask_from_bool(empty_is_enemy(modifier)) & self.empty;
-    enemy_mask |= mask_from_bool(ally_is_enemy(modifier)) & self.ally;
+    let mut empty = self.empty;
+    empty |= mask_from_bool(king_is_empty(modifier)) & self.ally_king;
+
+    let mut enemy = self.enemy;
+    enemy |= mask_from_bool(empty_is_enemy(modifier)) & self.empty;
+    enemy |= mask_from_bool(ally_is_enemy(modifier)) & self.ally;
+    enemy &= !(mask_from_bool(king_is_empty(modifier)) & self.ally_king);
 
     let mut moves = 0;
     let mut current = (dx, dy);
@@ -201,8 +220,8 @@ impl Board {
       current = (current.0 + dx, current.1 + dy);
 
       let within_board = new_move > 0;
-      let is_enemy = new_move & enemy_mask > 0;
-      let is_empty = new_move & self.empty > 0;
+      let is_enemy = new_move & enemy > 0;
+      let is_empty = new_move & empty > 0;
       let is_ally = !(is_empty || is_enemy);
 
       moves |= if_bool(is_enemy || is_empty, new_move, 0);
@@ -218,92 +237,72 @@ impl Board {
 
 //other
 impl Board {
-  fn gen_pin_path(&self, at_mask: u64, dx: i32, dy: i32) -> u64 {
-    let white_attackers = self.white.bishops | self.white.rooks | self.white.queens;
-    let black_attackers = self.black.bishops | self.black.rooks | self.black.queens;
-    let correct_pieces = if_mask(self.white_turn_mask, black_attackers, white_attackers);
+  pub(super) fn gen_pin_filter(&self, at_mask: u64) -> u64 {
+    let directions = [
+      (-1, 1, u64::MAX),
+      (0, 1, 0),
+      (1, 1, u64::MAX),
+      (-1, 0, 0),
+      (1, 0, 0),
+      (-1, -1, u64::MAX),
+      (0, -1, 0),
+      (1, -1, u64::MAX),
+    ];
 
     let mut pin_path = 0;
-    let mut current = (dx, dy);
+    let diag = self.enemy & (self.bishops() | self.queens());
+    let not_diag = self.enemy & (self.rooks() | self.queens());
 
-    let mut ally_count = 0;
-    let mut enemy_count = 0;
-    let mut pinned;
+    for (dx, dy, diag_mask) in directions {
+      let pinner = if_mask(diag_mask, diag, not_diag);
 
-    loop {
-      let mut new_move = if_bool(
-        dx > 0,
-        at_mask.move_right_mask(current.0.unsigned_abs()),
-        at_mask.move_left_mask(current.0.unsigned_abs()),
-      );
-      new_move = if_bool(
-        dy > 0,
-        new_move.move_up_mask(current.1.unsigned_abs()),
-        new_move.move_down_mask(current.1.unsigned_abs()),
-      );
+      let king_to_pin_path = self.gen_iterative_moves(self.ally_king, dx, dy, ALLY_IS_ENEMY);
+      let king_hits_pin = king_to_pin_path & at_mask > 0;
 
-      current = (current.0 + dx, current.1 + dy);
+      let pin_to_pinner_path = self.gen_iterative_moves(at_mask, dx, dy, NONE) | pinner;
+      let pin_hits_pinner = pin_to_pinner_path & pinner > 0;
 
-      let within_board = new_move > 0;
-      let is_enemy = new_move & self.enemy > 0;
-      let is_ally = new_move & self.ally > 0;
-
-      enemy_count += is_enemy as u64;
-      ally_count += is_ally as u64;
-
-      pinned = ally_count == 1 && enemy_count == 1 && (correct_pieces & new_move > 0);
-      let done =
-        (ally_count == 0 && enemy_count == 1) || (ally_count == 2 && enemy_count == 0) || pinned;
-
-      pin_path |= new_move;
-
-      if !within_board || done {
-        break;
-      }
+      let pinned = mask_from_bool(king_hits_pin & pin_hits_pinner);
+      pin_path = pinned & (king_to_pin_path | pin_to_pinner_path);
     }
 
-    if_bool(pinned, pin_path, 0)
-  }
-
-  pub(super) fn gen_pin_filter(&self, at_mask: u64) -> u64 {
-    let mut pin_filter = 0;
-
-    pin_filter |= self.gen_pin_path(self.ally_king, -1, -1);
-    pin_filter |= self.gen_pin_path(self.ally_king, 1, -1);
-    pin_filter |= self.gen_pin_path(self.ally_king, -1, 1);
-    pin_filter |= self.gen_pin_path(self.ally_king, 1, 1);
-    pin_filter |= self.gen_pin_path(self.ally_king, -1, 0);
-    pin_filter |= self.gen_pin_path(self.ally_king, 0, -1);
-    pin_filter |= self.gen_pin_path(self.ally_king, 1, 0);
-    pin_filter |= self.gen_pin_path(self.ally_king, 0, 1);
-
-    if_bool(pin_filter & at_mask > 0, pin_filter, u64::MAX)
+    //king not under attack? kinda discovered pin and king attack
+    if_bool(pin_path > 0, pin_path, u64::MAX)
   }
 
   pub(super) fn gen_check_filter(&self, at_mask: u64) -> u64 {
-    let enemy_pawns = if_mask(self.white_turn_mask, self.black.pawns, self.white.pawns);
-    let enemy_knights = if_mask(self.white_turn_mask, self.black.knights, self.white.knights);
-    let enemy_bishops = if_mask(self.white_turn_mask, self.black.bishops, self.white.bishops);
-    let enemy_rooks = if_mask(self.white_turn_mask, self.black.rooks, self.white.rooks);
-    let enemy_queens = if_mask(self.white_turn_mask, self.black.queens, self.white.queens);
+    let enemy_pawns = self.pawns() & self.enemy;
+    let enemy_knights = self.knights() & self.enemy;
 
     let mut filter = 0;
     filter |= self.gen_pawn_capturing_moves(self.ally_king, NONE) & enemy_pawns;
     filter |= self.gen_knight_moves(self.ally_king, NONE) & enemy_knights;
-    filter |= if_bool(at_mask == self.ally_king, u64::MAX, 0);
+    filter |= mask_from_bool(at_mask == self.ally_king);
 
-    let bishop_attacks = self.gen_bishop_moves(enemy_bishops | enemy_queens, ALLY_IS_ENEMY)
-      | enemy_bishops
-      | enemy_queens;
-    let king_as_bishop = self.gen_bishop_moves(self.ally_king, NONE) | self.ally_king;
-    let combined = bishop_attacks & king_as_bishop;
-    filter |= if_bool(combined & self.ally_king > 0, combined, 0);
+    let directions = [
+      (-1, 1, u64::MAX),
+      (0, 1, 0),
+      (1, 1, u64::MAX),
+      (-1, 0, 0),
+      (1, 0, 0),
+      (-1, -1, u64::MAX),
+      (0, -1, 0),
+      (1, -1, u64::MAX),
+    ];
 
-    let rook_attacks =
-      self.gen_rook_moves(enemy_rooks | enemy_queens, ALLY_IS_ENEMY) | enemy_rooks | enemy_queens;
-    let king_as_rook = self.gen_rook_moves(self.ally_king, NONE) | self.ally_king;
-    let combined = rook_attacks & king_as_rook;
-    filter |= if_bool(combined & self.ally_king > 0, combined, 0);
+    let diag = self.enemy & (self.bishops() | self.queens());
+    let not_diag = self.enemy & (self.rooks() | self.queens());
+
+    for (dx, dy, diag_mask) in directions {
+      let attacker = if_mask(diag_mask, diag, not_diag);
+
+      let king_to_attacker_path = self.gen_iterative_moves(self.ally_king, dx, dy, NONE);
+      let king_hits_attacker = mask_from_bool(king_to_attacker_path & attacker > 0);
+
+      filter |= king_hits_attacker & (king_to_attacker_path | attacker);
+    }
+
+    filter = mask_from_bool(at_mask == self.ally_king) ^ filter;
 
     if_bool(filter > 0, filter, u64::MAX)
   }
--- src/board/move_generation_modifiers.rs
+++ src/board/move_generation_modifiers.rs
@@ -1,6 +1,7 @@
 pub(super) const NONE: u64 = 0;
 pub(super) const EMPTY_IS_ENEMY: u64 = 1;
 pub(super) const ALLY_IS_ENEMY: u64 = 2;
+pub(super) const KING_IS_EMPTY: u64 = 4;
 
 pub(super) fn empty_is_enemy(value: u64) -> bool {
   value & EMPTY_IS_ENEMY > 0
@@ -8,4 +9,8 @@ pub(super) fn empty_is_enemy(value: u64) -> bool {
 
 pub(super) fn ally_is_enemy(value: u64) -> bool {
   value & ALLY_IS_ENEMY > 0
+}
+
+pub(super) fn king_is_empty(value: u64) -> bool {
+  value & KING_IS_EMPTY > 0
 }
